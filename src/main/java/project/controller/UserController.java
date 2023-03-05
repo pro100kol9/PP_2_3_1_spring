e74a0f99f5ee41192b405f030463c34789502239
nikonpavlenko <nikonpavlenko@gmail.com>
2023-03-05 14:06:37 +1100

    update

--- src/main/java/project/controller/UserController.java
+++ src/main/java/project/controller/UserController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import project.model.User;
 import project.service.UserService;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/users")
 public class UserController {
@@ -25,28 +23,32 @@ public class UserController {
         users.addAttribute("users", userService.getAllUser());
         return "home";
     }
+
     @GetMapping("/saveget")
-    public String saveUserGet(@ModelAttribute ("user") User user) {
+    public String saveUserGet(@ModelAttribute("user") User user) {
         return "newuser";
     }
-    @PostMapping ("/savepost")
-    public String saveUserPost(@ModelAttribute ("user") User user) {
+
+    @PostMapping("/savepost")
+    public String saveUserPost(@ModelAttribute("user") User user) {
         userService.saveUser(user);
         return "redirect:/users";
     }
+
     @DeleteMapping("/{id}")
-    public String deleteUserById(@PathVariable ("id") Long id) {
+    public String deleteUserById(@PathVariable("id") Long id) {
         userService.deleteUserById(id);
         return "redirect:/users";
     }
 
     @GetMapping("/usereditorget/{id}")
-    public String userEditorGet (Model user, @PathVariable ("id") Long id) {
+    public String userEditorGet(Model user, @PathVariable("id") Long id) {
         user.addAttribute("user", userService.getUserById(id));
         return "usereditor";
     }
+
     @PatchMapping("/{id}")
-    public String userEditorPatch (@ModelAttribute ("user") User user,  @PathVariable ("id") Long id) {
+    public String userEditorPatch(@ModelAttribute("user") User user, @PathVariable("id") Long id) {
         userService.userEditor(user, id);
         return "redirect:/users";
     }
